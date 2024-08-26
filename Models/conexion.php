033f623016b09927f2d22e27b49600a2446190a7
tearkive <evelyncorralejo2508@gmail.com>
2024-08-26 08:08:47 -0600

    Changes to be committed: 	modified:   Models/conexion.php 	modified:   Views/Informacion/principal.php

--- Models/conexion.php
+++ Models/conexion.php
@@ -16,7 +16,6 @@
 			$conn = mysqli_connect($this->host,$this->username, $this->password,$this->dbname);
 			if (!$conn) {
 				die("Connection failed: " . mysqli_connect_error());
-				return null;
 			}else{
 				//echo "Connected successfully";
 				return $conn;
--- Views/Informacion/principal.php
+++ Views/Informacion/principal.php
@@ -22,23 +22,23 @@
 	<main>
 		<div id="carouselExample" class="carousel slide">
 			<div class="carousel-inner">
-			<div class="carousel-item active">
-				<img src="../../Images/uno.png" class="d-block w-100" alt="imagen">
-			</div>
-			<div class="carousel-item">
-				<img src="../../Images/dos.png" class="d-block w-100" alt="imgen">
-			</div>
-			<div class="carousel-item">
-				<img src="../../Images/tres.png" class="d-block w-100" alt="imagen">
-			</div>
+				<div class="carousel-item active">
+					<img src="../../Images/uno.png" class="d-block w-100" alt="imagen">
+				</div>
+				<div class="carousel-item">
+					<img src="../../Images/dos.png" class="d-block w-100" alt="imgen">
+				</div>
+				<div class="carousel-item">
+					<img src="../../Images/tres.png" class="d-block w-100" alt="imagen">
+				</div>
 			</div>
 			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="visually-hidden">Previous</span>
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
 			</button>
 			<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-			<span class="carousel-control-next-icon" aria-hidden="true"></span>
-			<span class="visually-hidden">Next</span>
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
 			</button>
 		</div>
 		<div class="barra-color-primer"></div>
